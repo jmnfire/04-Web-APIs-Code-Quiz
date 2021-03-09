2e0a6d5dbb64693a775b468002334415c6ca491e
Jovan Nixon <jmnfire@gmail.com>
2021-03-08 23:14:57 -0700

    update3

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,8 +18,10 @@ function startGame() {
   shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-  
+  setNextQuestion();
+  setTime();
+  console.log("game start");
+
 }
 
 function setNextQuestion() {
@@ -80,55 +82,104 @@ function clearStatusClass(element) {
 }
 
 
-
 var questions = [
   {
-    question: 'What is 2+2?',
+    question: 'Inside which HTML element do we put the JavaScript?',
     answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false },
-      { text: '13', correct: false},
-      { text: '15', correct: false}
+      { text: 'Script', correct: true },
+      { text: 'div', correct: false },
+      { text: 'section', correct: false},
+      { text: 'scripting', correct: false}
     ]
   },
   {
-    question: 'Who is jovan',
+    question: 'Where is the correct place to insert a JavaScript?',
     answers: [
-      { text: 'ejhsfdjfhdsjh', correct: false },
-      { text: 'kjfklajkfjj', correct: false },
-      { text: 'fjdfjfdfjdfd', correct: true },
-      { text: 'fsdfhskfsdfjkds', correct: false }
+      { text: 'The Body section', correct: false },
+      { text: 'The Head section', correct: true },
+      { text: 'The div section', correct: false },
+      { text: 'Both the Head and the Body section?', correct: false }
     ]
   },
   {
-    question: 'What is 5?',
+    question: 'What does CSS stand for?',
     answers: [
-      {text: '4', correct: false },
-      {text: '22', correct: false },
-      { text: '13', correct: false },
-      { text: '15', correct: true }
+      {text: 'Colorful Style Sheets', correct: false },
+      {text: 'Computer Style Sheets', correct: false },
+      { text: 'Creative Style Sheets', correct: false },
+      { text: 'Cascading Style Sheets', correct: true }
     ]
   },
   {
-    question: 'What is 10?',
+    question: 'What does HTML stand for?',
     answers: [
-      {text: '4', correct: true },
-      {text: '22', correct: false },
-      { text: '13', correct: false },
-      { text: '15', correct: false }
+      {text: 'Hyper Text Markup Language', correct: true },
+      {text: 'Hurry Type My Language', correct: false },
+      { text: 'Heres The Markup Language', correct: false },
+      { text: 'Home Tool Markup Language', correct: false }
     ]
   },
   {
-    question: 'What is 100?',
+    question: 'Which HTML attribute is used to define inline styles?',
     answers: [
-      {text: '4', correct: false },
-      {text: '22', correct: true },
-      { text: '13', correct: false },
-      { text: '15', correct: false }
+      {text: 'font', correct: false },
+      {text: 'style', correct: true },
+      { text: 'styles', correct: false },
+      { text: 'class', correct: false }
     ]
   },
 ]
 
+// High Scores 
+var time = document.getElementById("timer");
+var yourScore = document.querySelector(".display-3");
+var submitButton = document.getElementById("buttonInitials");
+var inputLine = document.getElementById("inlineFormInput");
+
+var secondsLeft = 30;
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      console.log(secondsLeft);
+        time.textContent = "Time: " + secondsLeft;
+      
+        if(secondsLeft === 0) {
+          clearInterval(timerInterval);
+          yourScore.textContent = "Your score is: " + secondsLeft;
+          submitButton.setAttribute("style", "display: inline");
+          inputLine.setAttribute("style", "display: inline-block");
+      
+          } else if (question === 5) {
+            clearInterval(timerInterval);
+            console.log(secondsLeft);
+            yourScore.textContent = "Your score is: " + secondsLeft;
+            submitButton.setAttribute("style", "display: inline");
+            inputLine.setAttribute("style", "display: inline-block");
+
+          }
+        
+
+    }, 1000);
+  }
+var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+submitButton.addEventListener("click", function(event){
+  event.stopPropagation();
+  console.log("click");
+  
+  var initials = inputLine.value;
+  var finalScore = {initials, secondsLeft};
+  console.log("Final Score: " + finalScore);
+  console.log(initials + " your score is: " + secondsLeft); 
+
+
+
+
+  // Send to localStorage
+
+  highscores.push(finalScore);
+  localStorage.setItem("highscores", JSON.stringify(highscores));
+});
 
 // var gameTimer = document.querySelector('#timer');
 // var timerInterval = 1000;
