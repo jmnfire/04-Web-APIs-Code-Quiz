aa23b30facd7fc9e621177aafe6b47456b80b597
Jovan Nixon <jmnfire@gmail.com>
2021-03-10 22:10:36 -0700

    update4

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # 04-Web-APIs-Code-Quiz
+
+## Description 
+Purpose of this project is to create a timed JavaScript quiz, where users may save their score to their local storage after completing the coding quiz challenge by entering their initials. All the saved scores can be viewed on the high-scores page, where the user may choose to clear or save the scores. 
+
+The quiz is made up of 5 questions. The user has 25 seconds to compleat the quiz.  The game ends once the user hits 0 seconds or he answers all the questions and submits his instals. 
+
+## Table of Contents
+* [Features](#Features)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Badges](#Badges)
+
+## Features
+- setTime
+- Media query
+- JavaScript
+- Local Storage
+- DOM manipulation 
+- .remove, .forEach, array.from
+- JSON.parse
+
+
+
+## Installation 
+
+NO installation required you can view the page. If you want to look at my code, do a git clone or my repository. 
+
+[Link to website]()
+
+## Usage
+
+![screenshot](assets/images/screencapture-file-Users-JovanNixon-Desktop-Homework-02-Advanced-CSS-Portfolio-First-index-html-2021-02-26-21_47_28.png)
+
+
+## License 
+
+MIT License
+Copyright (c) 2021 Jovan Nixon
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Badges
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var startButton = document.getElementById('start-btn')
 var nextButton = document.getElementById('next-btn')
-var questionContainerElement = document.getElementById ('question-container')
+var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
 
@@ -66,7 +66,7 @@ function selectAnswer(e) {
     questionContainerElement.classList.add('hide')
     submitForm.classList.remove('hide')
   }
-  
+
 }
 
 function setStatusClass(element, correct) {
@@ -84,143 +84,156 @@ function clearStatusClass(element) {
 }
 
 
-var questions = [
-  {
+var questions = [{
     question: 'Inside which HTML element do we put the JavaScript?',
-    answers: [
-      { text: 'Script', correct: true },
-      { text: 'div', correct: false },
-      { text: 'section', correct: false},
-      { text: 'scripting', correct: false}
+    answers: [{
+        text: 'Script',
+        correct: true
+      },
+      {
+        text: 'div',
+        correct: false
+      },
+      {
+        text: 'section',
+        correct: false
+      },
+      {
+        text: 'scripting',
+        correct: false
+      }
     ]
   },
   {
     question: 'Where is the correct place to insert a JavaScript?',
-    answers: [
-      { text: 'The Body section', correct: false },
-      { text: 'The Head section', correct: true },
-      { text: 'The div section', correct: false },
-      { text: 'Both the Head and the Body section?', correct: false }
+    answers: [{
+        text: 'The Body section',
+        correct: false
+      },
+      {
+        text: 'The Head section',
+        correct: true
+      },
+      {
+        text: 'The div section',
+        correct: false
+      },
+      {
+        text: 'Both the Head and the Body section?',
+        correct: false
+      }
     ]
   },
   {
     question: 'What does CSS stand for?',
-    answers: [
-      {text: 'Colorful Style Sheets', correct: false },
-      {text: 'Computer Style Sheets', correct: false },
-      { text: 'Creative Style Sheets', correct: false },
-      { text: 'Cascading Style Sheets', correct: true }
+    answers: [{
+        text: 'Colorful Style Sheets',
+        correct: false
+      },
+      {
+        text: 'Computer Style Sheets',
+        correct: false
+      },
+      {
+        text: 'Creative Style Sheets',
+        correct: false
+      },
+      {
+        text: 'Cascading Style Sheets',
+        correct: true
+      }
     ]
   },
   {
     question: 'What does HTML stand for?',
-    answers: [
-      {text: 'Hyper Text Markup Language', correct: true },
-      {text: 'Hurry Type My Language', correct: false },
-      { text: 'Heres The Markup Language', correct: false },
-      { text: 'Home Tool Markup Language', correct: false }
+    answers: [{
+        text: 'Hyper Text Markup Language',
+        correct: true
+      },
+      {
+        text: 'Hurry Type My Language',
+        correct: false
+      },
+      {
+        text: 'Heres The Markup Language',
+        correct: false
+      },
+      {
+        text: 'Home Tool Markup Language',
+        correct: false
+      }
     ]
   },
   {
     question: 'Which HTML attribute is used to define inline styles?',
-    answers: [
-      {text: 'font', correct: false },
-      {text: 'style', correct: true },
-      { text: 'styles', correct: false },
-      { text: 'class', correct: false }
+    answers: [{
+        text: 'font',
+        correct: false
+      },
+      {
+        text: 'style',
+        correct: true
+      },
+      {
+        text: 'styles',
+        correct: false
+      },
+      {
+        text: 'class',
+        correct: false
+      }
     ]
   },
 ]
 
-// High Scores 
 var time = document.getElementById("timer");
-var yourScore = document.querySelector(".display-3");
+var yourScore = document.querySelector(".high-score");
 var submitButton = document.getElementById("buttonInitials");
 var inputLine = document.getElementById("inlineFormInput");
 
-var secondsLeft = 30;
+var secondsLeft = 25;
+
 function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    console.log(secondsLeft);
+    time.textContent = "Time: " + secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      yourScore.textContent = "Your score is: " + secondsLeft;
+      alert('Game Over!')
+      submitButton.setAttribute("style", "display: inline");
+      inputLine.setAttribute("style", "display: inline-block");
+
+    } else if (questions === 5) {
+      clearInterval(timerInterval);
       console.log(secondsLeft);
-        time.textContent = "Time: " + secondsLeft;
-      
-        if(secondsLeft === 0) {
-          clearInterval(timerInterval);
-          yourScore.textContent = "Your score is: " + secondsLeft;
-          submitButton.setAttribute("style", "display: inline");
-          inputLine.setAttribute("style", "display: inline-block");
-      
-          } else if (question === 5) {
-            clearInterval(timerInterval);
-            console.log(secondsLeft);
-            yourScore.textContent = "Your score is: " + secondsLeft;
-            submitButton.setAttribute("style", "display: inline");
-            inputLine.setAttribute("style", "display: inline-block");
+      yourScore.textContent = "Your score is: " + secondsLeft;
+      submitButton.setAttribute("style", "display: inline");
+      inputLine.setAttribute("style", "display: inline-block");
 
-          }
-        
+    }
 
-    }, 1000);
-  }
+
+  }, 1000);
+}
+
 var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
-submitButton.addEventListener("click", function(event){
+submitButton.addEventListener("click", function (event) {
   event.stopPropagation();
   console.log("click");
-  
+
   var initials = inputLine.value;
-  var finalScore = {initials, secondsLeft};
+  var finalScore = {
+    initials,
+    secondsLeft
+  };
   console.log("Final Score: " + finalScore);
-  console.log(initials + " your score is: " + secondsLeft); 
+  console.log(initials + " your score is: " + secondsLeft);
 
-
-
-
-  // Send to localStorage
 
   highscores.push(finalScore);
   localStorage.setItem("highscores", JSON.stringify(highscores));
 });
-
-// var gameTimer = document.querySelector('#timer');
-// var timerInterval = 1000;
-// var timerCount = 75;
-// var counter;
-
-
-
-// function gameStart() {
-  
-//   // Add event listener for when the game starts
-//   gameStartBtn.addEventListener('click', function() {
-
-//     timerCount = 75;
-
-//     updateTimer();
-
-//     // start a set interval to countdown to zero from the timerCount
-//     counter = setInterval(function() {
-    
-//       timerCount--;
-//       // If the timer count is zero, stop the counter
-//       if(timerCount === 0) {
-//         clearInterval(counter);
-//         alert('Times up!')
-//       }
-//       // Update the time shown remaining on the page
-//       updateTimer();
-//     }, timerInterval)
-//   })
-// }
-
-// function updateTimer() {
-//   gameTimer.textContent = timerCount;
-// }
-
-// function init() {
-//   gameStart();
-//   updateTimer();
-// }
-
-// init();
